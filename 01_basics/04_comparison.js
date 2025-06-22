e84740f55a28dc051d4105fb2874b0852aca356f
dakshpal23 <dakshpal290@gmail.com>
2025-06-22 08:24:14 +0000

    04 commiting

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+// console.log("2" > 1)//true (yha js ne "2" ko bhi no. mei convert kr diya) 
+// console.log(2 > "1")//true (here also)
+
+
+console.log(null>0)
+console.log(null>=0)//yha >= ne null ko eek n0. mei convert kr diya that's why its true
+
