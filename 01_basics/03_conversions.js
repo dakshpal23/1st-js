f39aa669380c692a9428b42b03c97ac07bdd5664
dakshpal23 <dakshpal290@gmail.com>
2025-06-22 07:57:58 +0000

    commiting conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,10 @@
+console.log("1" + 2)
+// string pehle hai to sbhi ko string ki trah treat kiya jayega 
+
+
+
+console.log(1 + 3 + "6")
+// Aur agr string last mei hai to starting ke operations ho jayenge
+
+console.log(1+2+"3"+5+2)
+//3352
--- 01_basics/test.js
+++
@@ -1,4 +0,0 @@
-// console.log("hitesh")
-
-let a = null;
-console.log(typeof(a));
