06e52e4c48d260f7bb8b22de384103f885363740
dakshpal23 <dakshpal290@gmail.com>
2025-06-25 17:25:31 +0000

    execution commit

--- 03_basics/05_execution.js
+++ 03_basics/05_execution.js
@@ -1,7 +1,13 @@
-/* Javascript Execution Context 
+/* JAVASCRIPT EXECUTION CONTEXT 
+
 > At first jb hum js ko code dete hai to eek Global Execution Context bnta hai jo THIS mei rkha jata hai
 > There are three types of execution context:
     - Global Execution Context
     - Function Execution Context
     - Eval Execution Context
-    */
+> In Global Execution Context there are two phases:
+    - Memory creation phase
+        ~ Sbhi variables aur functions load krega aur unhe undefined value assign krega
+    - Execution phase
+        ~ After Memory creation phase ye line to line execute krega code ko
+*/
